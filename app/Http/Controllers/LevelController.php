2934fbb29ff702a54b217f0adc8dc48b96a88c53
athaabiyyu <auliaatha123@gmail.com>
2024-03-12 19:39:40 +0700

    Eloquent ORM

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -9,8 +9,8 @@ class LevelController extends Controller
 {
     public function index() {
         // add data
-        // DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
-        // return 'Insert data baru berhasil';
+        DB::insert('insert into m_level(level_kode, level_nama, created_at) values(?, ?, ?)', ['CUS', 'Pelanggan', now()]);
+        return 'Insert data baru berhasil';
 
         // update data
         // $row = DB::update('update m_level set level_nama = ? where level_kode = ?', ['Customer', 'CUS',]);
@@ -21,7 +21,7 @@ class LevelController extends Controller
         // return 'Delete data berhasil. Jumlah data yang dihapus : ' . $row . ' baris';
 
         // show data
-        $data = DB::select('select * from m_level');
-        return view('level', ['data' => $data]);
+        // $data = DB::select('select * from m_level');
+        // return view('level', ['data' => $data]);
     }
 }
