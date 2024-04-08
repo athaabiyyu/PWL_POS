d60df63f82f8c5871580ea8412515396e0989778
athaabiyyu <auliaatha123@gmail.com>
2024-04-09 01:29:25 +0700

    $fillable

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,22 +9,16 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index() {
-
-        // insert data 
-        // $data  = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 3
-        // ];
-        // UserModel::insert($data);
-
-        // insert data
-        $data = [
-            'nama' => 'Pelanggan Pertama'
+    public function index()
+    {
+        // create data
+        $data  = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data' => $user]);
