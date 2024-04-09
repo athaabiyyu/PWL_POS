d9e9eff90c18e7fca1e2e659735f7262299dc9df
athaabiyyu <auliaatha123@gmail.com>
2024-04-09 11:56:31 +0700

    Retreiving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,7 +7,7 @@
      <title>Data User</title>
 </head>
 <body>
-     {{-- <h1>Data User</h1>
+     <h1>Data User</h1>
      <table border="1" cellpadding="2" cellspacing="0">
           <tr>
                <th>ID</th>
@@ -20,14 +20,6 @@
                <td>{{ $data->username }}</td>
                <td>{{ $data->nama }}</td>
                <td>{{ $data->level_id }}</td>
-          </tr>
-     </table> --}}
-     <table border="1" cellpadding="2" cellspacing="0">
-          <tr>
-               <th>Jumlah Pengguna</th>
-          </tr>
-          <tr>
-               <td>{{ $data }}</td>
           </tr>
      </table>
 </body>
