8d781007f4ca38eb41ec04eaac97a0db770321fd
athaabiyyu <auliaatha123@gmail.com>
2024-04-12 00:10:57 +0700

    Integrasi Laravel dengan AdminLte3 & Integrasi dengan DataTables

--- database/seeders/KategoriSeeder.php
+++ database/seeders/KategoriSeeder.php
@@ -17,28 +17,13 @@ class KategoriSeeder extends Seeder
         $data = [
             [
                 'kategori_id' => 1,
-                'kategori_kode' => 'ktg01',
-                'kategori_nama' => 'Perabotan',
+                'kategori_kode' => 'CML',
+                'kategori_nama' => 'Cemilan',
             ],
             [
                 'kategori_id' => 2,
-                'kategori_kode' => 'ktg02',
-                'kategori_nama' => 'Pecah Belah',
-            ],
-            [
-                'kategori_id' => 3,
-                'kategori_kode' => 'ktg03',
-                'kategori_nama' => 'Elektronik',
-            ],
-            [
-                'kategori_id' => 4,
-                'kategori_kode' => 'ktg04',
-                'kategori_nama' => 'Pakaian',
-            ],
-            [
-                'kategori_id' => 5,
-                'kategori_kode' => 'ktg05',
-                'kategori_nama' => 'Audio',
+                'kategori_kode' => 'MNR',
+                'kategori_nama' => 'Minuman Ringan',
             ],
         ];
         DB::table('m_kategori')->insert($data);
