6ad5c3ead351772abd1a004ff20a8b1d99f72f02
athaabiyyu <auliaatha123@gmail.com>
2024-04-20 19:17:59 +0700

    Penerapan Layouting

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -9,9 +9,9 @@
      <!-- Google Font: Source Sans Pro -->
      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
      <!-- Font Awesome -->
-     <link rel="stylesheet" href="{{ assets('adminlte/plugins/fontawesome-free/css/all.min.css') }} ">
+     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }} ">
      <!-- Theme style -->
-     <link rel="stylesheet" href="{{ assets('adminlte/dist/css/adminlte.min.css') }} ">
+     <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }} ">
 </head>
 
 <body class="hold-transition sidebar-mini">
@@ -26,7 +26,7 @@
           <aside class="main-sidebar sidebar-dark-primary elevation-4">
                <!-- Brand Logo -->
                <a href="{{ url('/') }}" class="brand-link">
-                    <img src="{{ assets('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
+                    <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
                          class="brand-image img-circle elevation-3" style="opacity: .8">
                     <span class="brand-text font-weight-light">AdminLTE 3</span>
                </a>
@@ -54,11 +54,11 @@
      <!-- ./wrapper -->
 
      <!-- jQuery -->
-     <script src="{{ assets('adminlte/plugins/jquery/jquery.min.js') }}"></script>
+     <script src="{{ asset('adminlte/plugins/jquery/jquery.min.js') }}"></script>
      <!-- Bootstrap 4 -->
-     <script src="{{ assets('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+     <script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
      <!-- AdminLTE App -->
-     <script src="{{ assets('adminlte/dist/js/adminlte.min.js') }}"></script>
+     <script src="{{ asset('adminlte/dist/js/adminlte.min.js') }}"></script>
 
      {{-- <!-- AdminLTE for demo purposes -->
      <script src="../../dist/js/demo.js"></script> --}}
